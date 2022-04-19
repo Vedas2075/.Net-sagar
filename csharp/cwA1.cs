39ee7a974f4b9e896cf6f27a57e9e8df48c0d558
sagar shrestha <vc75it12@vedascollege.edu.np>
2022-04-19 23:37:33 +0545

    classwork

--- csharp/cwA1.cs
+++ csharp/cwA1.cs
@@ -7,15 +7,15 @@ void comparision()
 
     if(a>b){
         if(a>c){
-            Console.WriteLine("a is gretest");
+            Console.WriteLine("a is greatest");
         }
         else{
             Console.WriteLine("c is greater");
         }
         else if(b>c){
-            Console.Write("b is the largest");
+            Console.Write("b is the greatest");
         else{
-         Console.Write("c is the largest");
+         Console.Write("c is the greater");
             }
         }
     }
