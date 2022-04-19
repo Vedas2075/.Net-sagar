2b3b25396ca85affadfde7eaadd53232adcf8d59
sagar shrestha <vc75it12@vedascollege.edu.np>
2022-04-19 11:56:56 +0545

    add

---
+++ csharp/lession a2 array.cs
@@ -0,0 +1,26 @@
+class array{ 
+    
+    //1D
+    byte[] ages = new byte[100];
+
+    string[] names ={"bishnu","ram","shyam","hari","krishna","bahadur"};
+
+
+    //2D
+    string[,] chessboard = new string[8,8];
+
+    string[,] chessboard = 
+
+
+//jagged array
+bool[][] data = new bool[5][];
+
+void assignvariable(){
+    data[0] = new bool[3]{true, false, false};
+    data[1] = new bool[2]{true, false};    
+}
+
+
+
+
+}
