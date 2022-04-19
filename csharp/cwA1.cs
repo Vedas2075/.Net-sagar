a1942fb6df1efde7b9ca89c6998bcca699efacce
sagar shrestha <vc75it12@vedascollege.edu.np>
2022-04-19 12:04:53 +0545

    classwork

---
+++ csharp/cwA1.cs
@@ -0,0 +1,22 @@
+
+void comparision()
+{
+    a=20;
+    b=30;
+    c=41;
+
+    if(a>b){
+        if(a>c){
+            Console.WriteLine("Number one is gretest");
+        }
+        else{
+            Console.WriteLine("Number three is greater");
+        }
+        else if(b>c){
+            Console.Write("Number two is the largest");
+        else{
+         Console.Write("Number three is the largest");
+            }
+        }
+    }
+}
