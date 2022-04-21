660815f298110f23d78d547afb175508a2bacf01
sagar shrestha <vc75it12@vedascollege.edu.np>
2022-04-21 11:17:52 +0545

    Basic calculator

---
+++ csharp/LessionD4ClassesAndObject.cs
@@ -0,0 +1,29 @@
+// Desigh a basic Calculator
+
+namespase ABC;
+
+Public Class BasicCalcutator()
+{
+    public BasicCalculator()
+    {
+
+    }
+    public calculator(double op1, double op2)
+    {
+        operand1 = op1;
+        operand2 = op2;
+    }
+
+     public static string brand ="Boat";
+     
+     double operand1;
+     double operand2;
+
+    public double Sum()=> operand1+operand2;
+    public double Subtract()=> operand1-operand2;
+    public double Multiply()=> operand1*operand2;
+    public double Divide()=> operand1/operand2;
+
+
+
+}
